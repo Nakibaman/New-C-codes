dae1f94c7b485106101d9929668a0ceaf707c9be
Nakibaman <nakibaman@gmail.com>
2019-03-15 10:50:30 +0600

    update new
    update new

---
+++ 12.03.2019/dynamic array input/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int n;
+    int i;
+    printf("Enter number of students:");
+    scanf("%d",&n);
+    int bng_mrks[n];
+    printf("Enter bangla marks of %d students",n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",bng_mrks[i]);
+    }
+    return 0;
+}
---
+++ 12.03.2019/zero_array/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a[500000000]={0};
+    int i;
+    for(i=0;i<=499;i++)
+    printf("%d",a[i]);
+    return 0;
+}
