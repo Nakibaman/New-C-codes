d4fab1b14b3c09aa678601da5c1c463e0618bb58
Nakibaman <nakibaman@gmail.com>
2019-05-10 11:06:29 +0600

    update
    update

---
+++ 10.05.19/run_time_string/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char name[20];
+    printf("Amar naam ta jano ki chilo?");
+    gets(name);
+    printf("My name is - ");
+    puts(name);
+    return 0;
+}
---
+++ 10.05.19/string/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char name[6]={'N','A','K','I','B','\0'};
+    printf("My name is - ");
+    puts(name);
+    return 0;
+}
