580d84fc723920def96caf4e6c8d7c256d22762f
Nakibaman <nakibaman@gmail.com>
2019-05-17 10:05:51 +0600

    Lab Update
    Lab Update

--- 17.05.2019/TestGit/main.c
+++ 17.05.2019/TestGit/main.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <string.h>
 int main()
 {
-    printf("Hello world!\n");
+    char name[10]={'N','a','k','\0'};
+    printf("%s",name);
+    int n;
+    n=strlen(name);
+    printf("\n%d",n);
     return 0;
 }
