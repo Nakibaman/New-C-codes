0d6094ebc3114cc4d136c108e3301ccfaf522c53
Nakibaman <nakibaman@gmail.com>
2019-05-24 10:45:58 +0600

    update 2
    update 2

--- 24.05.2019/array2/main.c
+++ 24.05.2019/array2/main.c
@@ -3,6 +3,19 @@
 
 int main()
 {
-    printf("Hello world!\n");
+    int i;
+    int a[5]={2,-3,0,-9,-4};
+    printf("Positive numbers are:");
+    for(i=0;i<5;i++)
+    {
+    if(a[i]>=0)
+        printf("%d ",a[i]);
+    }
+    printf("\n Negative numbers are:");
+    for(i=0;i<5;i++)
+    {
+    if(a[i]<0)
+        printf("%d ",a[i]);
+    }
     return 0;
 }
